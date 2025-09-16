7acc8cc409296e56c1afe6c3f958e3a8beb2123e
yonamaiah-dot <yonamaiah@gmail.com>
2025-09-16 11:42:50 -0300

    sla

--- exemplo.c
+++ exemplo.c
@@ -1,6 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+#include <math.h>
+
+#include "raylib/raylib.h"
+#include "raylib/raylib/raymath.h"
+
+#define TELA_LARGURA 960
+#define TELA_ALTURA 640
 
 int main(void){
+
 
 }
